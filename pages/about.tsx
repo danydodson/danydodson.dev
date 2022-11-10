b9288e953b252a308c62b1a7c981b90c2fc71d10
Dany Dodson <danydodson@gmail.com>
2022-11-09 21:08:29 -0600

    :rocket: initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,97 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import Container from 'components/Container'
+import avatar from 'public/avatar.png'
+import avatarBW from 'public/avatar-bw.png'
+
+export default function About() {
+  return (
+    <Container title="About - Dany Dodson">
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+          About Me
+        </h1>
+        <div className="mb-8 prose dark:prose-dark leading-6">
+          <h2>Links</h2>
+          <ul>
+            <li>
+              Twitter: <a href="https://twitter.com/danydodson">@danydodson</a>
+            </li>
+            <li>
+              GitHub: <a href="https://github.com/danydodson">@danydodson</a>
+            </li>
+            <li>
+              Website: <Link href="https://danydodson.dev">https://danydodson.dev</Link>
+            </li>
+            <li>
+              LinkedIn:{' '}
+              <a href="https://www.linkedin.com/in/danydodson/">
+                https://www.linkedin.com/in/danydodson
+              </a>
+            </li>
+          </ul>
+          <h2>Bio</h2>
+          <h3>Job Title</h3>
+          <p>Dany Dodson, VP of Developer Experience at Vercel</p>
+          <h3>Long, 3rd Person</h3>
+          <p>
+            Dany Dodson is the VP of Developer Experience at{' '}
+            <a href="http://vercel.com/">Vercel</a>, where he helps developers
+            build a faster web and leads the Next.js community. He leads the
+            community for the React framework Next.js and is an open-source
+            contributor. An educator, writer, and speaker, Lee has created
+            courses on React, Next.js, and web development.
+          </p>
+          <h3>Long, 1st Person</h3>
+          <p>
+            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
+            <a href="https://vercel.com/">Vercel</a>, where my team helps
+            developers build a faster web. I'm a Next.js contributor and help
+            lead our open-source communities. I'm passionate about frontend
+            development and have created courses on React, Next.js, and web
+            development. I'm an advisor and investor in early stage startups.
+          </p>
+          <h3>Short, 3rd Person</h3>
+          <p>
+            Dany Dodson is the VP of Developer Experience at{' '}
+            <a href="http://vercel.com/">Vercel</a>, where he helps developers
+            build a faster web.
+          </p>
+          <h3>Short, 1st Person</h3>
+          <p>
+            Hey, I'm Lee. I'm the VP of Developer Experience at{' '}
+            <a href="https://vercel.com/">Vercel</a>, where my team helps
+            developers build a faster web.
+          </p>
+          <h3>Education</h3>
+          <p>
+            Dany Dodsongraduated from Iowa State University with a B.S. in
+            Computer Engineering.
+          </p>
+          <h2>Headshots</h2>
+          <div className="flex space-x-8">
+            <a href="/avatar.png">
+              <Image
+                alt="Dany Dodson headshot"
+                width={400}
+                quality={100}
+                src={avatar}
+                className="rounded-md"
+              />
+            </a>
+            <a href="/avatar-bw.jpg">
+              <Image
+                alt="Dany Dodson headshot"
+                width={400}
+                quality={100}
+                src={avatarBW}
+                className="rounded-md"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
