a0cf0801d29ab1d4d4ffc17642511b6cce6f9a60
danydodson <danydodson@gmail.com>
2023-12-26 17:25:59 -0600

    :sparkles: Added an omg.lol proof claim to a head element.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document(props) {
   return (
     <Html lang='en'>
       <Head>
+        <meta content="proven166fe5" name="omg-lol-proof-claim" />
         <link href='/static/favicons/favicon.ico' rel='shortcut icon' />
         <link href='/static/favicons/site.webmanifest' rel='manifest' />
         <link href='/static/favicons/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180' />
